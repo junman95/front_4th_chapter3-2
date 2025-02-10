22fb5498ac86b218853c2f48c05dd57d043d98fb
JunmanChoi <junman9595@gmail.com>
2025-02-10 23:48:51 +0900

    feat: 반복 설정 순서 변경

--- src/App.tsx
+++ src/App.tsx
@@ -180,13 +180,6 @@ function App() {
           </FormControl>
 
           <FormControl>
-            <FormLabel>반복 설정</FormLabel>
-            <Checkbox isChecked={isRepeating} onChange={(e) => setIsRepeating(e.target.checked)}>
-              반복 일정
-            </Checkbox>
-          </FormControl>
-
-          <FormControl>
             <FormLabel>알림 설정</FormLabel>
             <Select
               value={notificationTime}
@@ -200,6 +193,12 @@ function App() {
             </Select>
           </FormControl>
 
+          <FormControl>
+            <FormLabel>반복 설정</FormLabel>
+            <Checkbox isChecked={isRepeating} onChange={(e) => setIsRepeating(e.target.checked)}>
+              반복 일정
+            </Checkbox>
+          </FormControl>
           {isRepeating && (
             <VStack width="100%">
               <FormControl>
